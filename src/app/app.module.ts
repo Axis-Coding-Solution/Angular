4270aadbeac350c3e4488038e2f35c7b1a7093d9
Saqib Ali <msaqibali.2001@gmail.com>
2023-05-04 14:35:08 +0500

    HomePage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,37 +4,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material-module';
-import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from '@angular/common/http';
-import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
 import { CarouselComponent } from './carousel/carousel.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
-import { TestimonialsComponent } from './testimonials/testimonials.component';
-import { TestimonialsFeedbackSliderComponent } from './testimonials-feedback-slider/testimonials-feedback-slider.component';
 import { SwiperModule } from 'swiper/angular';
-import { ServicesComponent } from './services/services.component';
-import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent,
     HomeComponent,
     HeaderComponent,
     NavbarComponent,
-    ContactComponent,
     FooterComponent,
-    CarouselComponent,
-    PortfolioComponent,
-    TestimonialsComponent,
-    TestimonialsFeedbackSliderComponent,
-    ServicesComponent,
-    ErrorComponent
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
