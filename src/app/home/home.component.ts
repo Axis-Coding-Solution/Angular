2d1e15bf50eb8b1d44e9fc807400bc1c57ce2995
Hassankhattakk <73868020+Bilalkhankk@users.noreply.github.com>
2023-05-04 19:47:33 +0500

    Consulting Services Working

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,22 @@
 import { Component } from '@angular/core';
 
 
-interface Food {
-  value: string;
-  viewValue: string;}
+interface Consulting_Services {
+  Title: string;
+  Subtitle: string;}
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  foods: Food[] = [
-    {value: 'steak-0', viewValue: 'Steak'},
-    {value: 'pizza-1', viewValue: 'Pizza'},
-    {value: 'tacos-2', viewValue: 'Tacos'},
+  Consulting_Services: Consulting_Services[] = [
+    {Title: 'Developing a profitability strategy', Subtitle: 'It includes a business model, a plan of strategic steps for the transition to it, and the consolidation of the financial model in the future.'},
+    {Title: 'Optimization of company structure', Subtitle: 'Step-by-step development of the top-level company’s organizational structure, principles of distribution of functions and powers.'},
+    {Title: 'steak-0', Subtitle: 'Steak'},
+    {Title: 'steak-0', Subtitle: 'Steak'},
+    {Title: 'steak-0', Subtitle: 'Steak'}
   ];
   title = '2nd-Project';
 }
+  
