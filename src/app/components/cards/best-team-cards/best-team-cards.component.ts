ed6044d0a53fbd043a9f876720d52c1abffbdcb9
Hassankhattakk <73868020+Bilalkhankk@users.noreply.github.com>
2023-05-04 20:34:43 +0500

    best team card made

--- src/app/components/cards/best-team-cards/best-team-cards.component.ts
+++ src/app/components/cards/best-team-cards/best-team-cards.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+interface bestteam{
+  Title: string;
+  Subtitle: string;
+  svgImg: string;
+ 
+
+}
 @Component({
   selector: 'app-best-team-cards',
   templateUrl: './best-team-cards.component.html',
   styleUrls: ['./best-team-cards.component.scss']
 })
 export class BestTeamCardsComponent {
-
+bestteam: bestteam[]=[
+  { Title: 'William Blanchard', Subtitle:' CEO & Company founder', svgImg:'https://demo6.clientica.org/wp-content/uploads/2020/12/Image-481.jpg', },
+  { Title: 'William Blanchard', Subtitle:' CEO & Company founder', svgImg:'https://demo6.clientica.org/wp-content/uploads/2020/12/Image-491.jpg', },
+  { Title: 'William Blanchard', Subtitle:' CEO & Company founder', svgImg:'https://demo6.clientica.org/wp-content/uploads/2020/12/Image-501.jpg', },
+  { Title: 'William Blanchard', Subtitle:' CEO & Company founder', svgImg:'https://demo6.clientica.org/wp-content/uploads/2020/12/Image_4.png', },
+]
 }
