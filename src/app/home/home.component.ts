b10bfd7bcd1a3f64d325e3e60c3a0c00b2d9daee
Saqib Ali <msaqibali.2001@gmail.com>
2023-05-05 15:08:28 +0500

    PerformanceCard

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,6 +12,11 @@ interface ConsultingServicesCards{
   Shorttext: string;
   Longtext: string;
 }
+interface PerformanceCard{
+  Title: string;
+  Operand: string;
+  Subtitle: string;
+}
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -29,6 +34,12 @@ export class HomeComponent {
     {Title: 'Organization of strategic control', Shorttext: 'from $389', Longtext:'we always use effective and key indicators to systematically monitor the implementation of our strategy for your business.' },
     {Title: 'Selection of suitable personnel', Shorttext: 'from $199', Longtext:'Formation of a team with good theoretical and practical training, professional knowledge and high motivation for success.' },
     {Title: 'Effective support for your business', Shorttext: 'from $889', Longtext:'Solving various issues that arise in the course of the company’s activities. Prompt advice, timely advice and strategies.' }
+  ]
+  PerformanceCard: PerformanceCard[]=[
+    {Title:'91', Operand:'%', Subtitle:'Our developments bring income to companies'},
+    {Title:'85', Operand:'+', Subtitle:'Million dollars total economic effect for our clients'},
+    {Title:'50', Operand:'%', Subtitle:'Discount on our services is provided to VIP clients'},
+    {Title:'24', Operand:'/7', Subtitle:'In this mode our specialists answer your questions'}
   ]
   title = '2nd-Project';
 }
